6280f9c1955a061187ae60ce76fca4a9e4909cf2
Yu Wakui <wak121.y@gmail.com>
2024-09-26 06:57:14 +0900

    Initial Commit

---
+++ CoreLocationExample/CoreLocationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationExampleApp.swift
+//  CoreLocationExample
+//
+//  Created by Yu Wakui on 2024/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
